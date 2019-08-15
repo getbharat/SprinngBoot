ab2fabee544538e80a330571f817134e4349e192
BB019 <bhooshan.bharat90@gmail.com>
2019-08-15 22:41:27 +0530

    Home pages added

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping("/")
+	public ModelAndView home()
+	{
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("home");
+		return mv;
+		
+	}
+
+}
